f54f05619174592e5e4aacd58f3a57d1a6258d05
rupamkerketta <kerkettarudolf@gmail.com>
2018-11-09 00:20:17 +0530

    Updates

---
+++ precache-manifest.f4514cfc33207806ec303cfcb7390f53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "855df1d8b3b172117c20",
+    "url": "/ReactCalculator/static/js/runtime~main.855df1d8.js"
+  },
+  {
+    "revision": "8d6072dda8bc5b31665d",
+    "url": "/ReactCalculator/static/js/main.8d6072dd.chunk.js"
+  },
+  {
+    "revision": "41e8c3be725e232c4293",
+    "url": "/ReactCalculator/static/js/1.41e8c3be.chunk.js"
+  },
+  {
+    "revision": "8d6072dda8bc5b31665d",
+    "url": "/ReactCalculator/static/css/main.25a86ab7.chunk.css"
+  },
+  {
+    "revision": "c81e7d92df1ca8086a4a7fef3bf2c5d6",
+    "url": "/ReactCalculator/index.html"
+  }
+];
